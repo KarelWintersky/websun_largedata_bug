c26fb1bee986ec777a2c470677a669b346d564e4
Karel <karel.wintersky@gmail.com>
2018-08-13 20:00:25 +0300

    + 3 варианта шаблона

--- render.php
+++ render.php
@@ -9,18 +9,19 @@ $GLOBAL_DATA = preload_data('data/mock_data.json');
 
 $get_limit = intval($_GET['limit'] ?? 0 );
 
-echo "Count : {$get_limit}<hr>" . PHP_EOL;
 
 $authors_list = Load_Data($get_limit);
 
 $template_dir = "template/";
-$template_file_name = "authors__all.html";
+$template_file_name = "authors__all__4.html";
 $inner_html_data = [
     'all_authors_count'     => count($authors_list),
     'all_authors_list'      => $authors_list
 ];
 
 $return = websun_parse_template_path($inner_html_data, $template_file_name /* , $template_dir*/);
+
+echo "Count : {$get_limit}, used template `{$template_file_name}` <hr>" . PHP_EOL;
 
 echo $return;
 
